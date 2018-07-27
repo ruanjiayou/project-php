831166808a6650813a2cb3e4a4ca0deb22c3b8c1
ruanjiayou <1439120442@qq.com>
2018-07-27 15:50:20 +0800

    调整route,姿势更舒服

--- application/routes/test.php
+++ application/routes/test.php
@@ -3,11 +3,17 @@ use think\Request;
 use think\Response;
 
 return [
+  'get /test' => function($req, $res) {
+    return 'Hello World!';
+  },
+  'get /test1' => function($req, $res) {
+    return json(['name'=>'test']);
+  },
   /**
-   * @api {get} /test
+   * @api {get} /test/req
    * @apiName test
    * @apiGroup test
-   * @apiDescription 访问地址/test?page=1&limit=2&order=id-DESC&search=搜索,返回paging()处理后的json
+   * @apiDescription 访问地址/test/req?page=1&limit=2&order=id-DESC&search=搜索,返回paging()处理后的json
    * 
    * @apiSuccessExample Success-Response:
    * HTTP/1.1 200 OK
@@ -18,10 +24,21 @@ return [
    *   search: '搜索'
    * }
    */
-  'get /test' => function(Request $req, Response $res) {
-    header('Content-Type:application/json; charset=utf8');
+  'get /test/req/paging' => function($req, $res) {
     $result = $req->paging();
-    return $res->return($result);
-  }
+    $res->return($req->paging());
+  },
+  'get /test/res/return' => function($req, $res) {
+    $res->return(['test']);
+  },
+  'get /test/res/success' => function($req, $res) {
+    $res->success();
+  },
+  'get /test/res/fail' => function($req, $res) {
+    $res->fail();
+  },
+  'get /test/res/paging' => function($req, $res) {
+    $res->paging(['test']);
+  },
 ]
 ?>
