1032273ef472b42b39d0afcd2e730d4e5640a654
ruanjiayou <1439120442@qq.com>
2018-08-02 10:24:24 +0800

    修改文档

--- application/routes/v1/user/rccode.php
+++ application/routes/v1/user/rccode.php
@@ -13,12 +13,19 @@ return [
    * @apiSuccessExample Success-Response:
    * HTTP/1.1 200 OK
    * {
-   *   status: 'success',
-   *   result: {
+   *   state: 'success',
+   *   edata: {
    *     id: 1,
-   *     phone: '18888888888',
+   *     agencyId: 1,
+   *     agencyName: 'max',
+   *     agencyAvatar: '',
+   *     rccode: '',
+   *     type: 'pending',
    *     createdAt: "2018-07-31 17:43:48"
-   *   }
+   *   },
+   *   ecode: 0,
+   *   error: '',
+   *   stack: ''
    * }
    */
   'post /v1/user/rccode' => function($req, $res) {
@@ -29,6 +36,69 @@ return [
     $result = RccodeBLL::create($user);
     $res->return($result);
   },
+  /**
+   * @api {delete} /v1/user/rccode/:userId 中介删除合作关系,暂时不允许此项操作
+   * @apiGroup user-rccode
+   * 
+   * @apiHeader {string} token 鉴权
+   * 
+   * @apiSuccessExample Success-Response:
+   * HTTP/1.1 200 OK
+   * {
+   *   state: 'fail',
+   *   edata: null,
+   *   ecode: 0,
+   *   error: '',
+   *   stack: ''
+   * }
+   */
+  'delete /v1/user/rccode/:userId' => function($req, $res) {
+    $user = UserBLL::auth($req);
+    $result = [];
+    $res->fail();
+  },
+  /**
+   * @api {get} /v1/user/rccode 获取合作人列表
+   * @apiGroup user-rccode
+   * 
+   * @apiHeader {string} token 鉴权
+   * 
+   * @apiSuccessExample Success-Response:
+   * HTTP/1.1 200 OK
+   * {
+   *   state: 'success',
+   *   edata: [{
+   *     id: 1,
+   *     agencyId: 1,
+   *     agencyName: 'max',
+   *     agencyAvatar: '',
+   *     rccode: '',
+   *     type: 'pending',
+   *     createdAt: "2018-07-31 17:43:48"
+   *   }],
+   *   ecode: 0,
+   *   error: '',
+   *   stack: '',
+   *   pagination: {
+   *     page: 1,
+   *     pages: 1,
+   *     limit: 10,
+   *     count: 1,
+   *     total: 1
+   *   }
+   * }
+   */
+  'get /v1/user/rccode' => function($req, $res) {
+    $user = UserBLL::auth($req);
+    $hql = $req->paging();
+    if($user['type']==='agency') {
+      $hql['where']['agencyId'] = $user['id'];
+    } else {
+      $hql['where']['userId'] = $user['id'];
+    }
+    $users = model('rccode')->getList($hql);
+    $res->paging($users);
+  }
 ];
 
 ?>
