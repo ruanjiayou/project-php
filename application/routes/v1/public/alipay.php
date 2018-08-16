f70c6472dbfafaf2c129e347367f57699570d984
ruanjiayou <1439120442@qq.com>
2018-08-16 15:51:46 +0800

    调试支付回调

--- application/routes/v1/public/alipay.php
+++ application/routes/v1/public/alipay.php
@@ -3,14 +3,9 @@ use think\Log;
 return [
   'post /v1/public/alipay-cb' => function($req, $res) {
     $data = input('post.');
-    Log::write($data);
-    dump($data);
+    Log::write(json_encode($data));
     $flag = alipayHelper::appPayCb($data);
-    if($flag) {
-      $res->success();
-    } else {
-      $res->fail();
-    }
+    return $flag ? 'success' : 'fail';
   }
 ];
 ?>
