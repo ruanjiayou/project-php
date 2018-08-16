736b369bab064067b2b259c7f1de439ddb684645
ruanjiayou <1439120442@qq.com>
2018-08-16 16:12:22 +0800

    支付回调

--- application/routes/v1/public/alipay.php
+++ application/routes/v1/public/alipay.php
@@ -3,8 +3,14 @@ use think\Log;
 return [
   'post /v1/public/alipay-cb' => function($req, $res) {
     $data = input('post.');
-    Log::write(json_encode($data));
-    $flag = alipayHelper::appPayCb($data);
+    $orderBLL = new OrderBLL();
+    $order = $orderBLL->getInfo(['order_no'=>$data['out_trade_no']]);
+    //$flag = alipayHelper::appPayCb($data);
+    if(null === $order) {
+      return 'fail';
+    } else {
+      $orderBLL->update(['trade_no'=>$data['trade_no'],'status'=>'success'], ['id'=>$order['id']]);
+    }
     return $flag ? 'success' : 'fail';
   }
 ];
