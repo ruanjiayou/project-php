9531af6c223c9e633aa9a3d77f3f745cd2bdd78b
ruanjiayou <1439120442@qq.com>
2018-09-12 09:44:32 +0800

    消息推送测试

--- application/routes/test.php
+++ application/routes/test.php
@@ -3,6 +3,23 @@ use app\model;
 use think\Request;
 use think\Response;
 
+function IGtNotificationTemplateDemo($APPID, $APPKEY){
+  $template =  new IGtNotificationTemplate();
+  $template->set_appId($APPID);                   //应用appid
+  $template->set_appkey($APPKEY);                 //应用appkey
+  $template->set_transmissionType(1);            //透传消息类型
+  $template->set_transmissionContent("测试离线");//透传内容
+  $template->set_title("请填写通知标题");      //通知栏标题
+  $template->set_text("请填写通知内容");     //通知栏内容
+  $template->set_logo("");                       //通知栏logo
+  $template->set_logoURL("");                    //通知栏logo链接
+  $template->set_isRing(true);                   //是否响铃
+  $template->set_isVibrate(true);                //是否震动
+  $template->set_isClearable(true);              //通知栏是否可清除
+
+  return $template;
+}
+
 return [
   /**
    * @api {get} /test 1.测试返回字符串
@@ -298,6 +315,55 @@ return [
   'get /test/route-pattern' => function($req, $res) {
     $r = CustomRoute::getRoutePattern('get /admin/:adminId([0-9]+)/auth/:name(\w+)');
     $res->return($r);
+  },
+  /**
+   * @api {post} /test/getui 测试个推接口推送APP消息
+   * @apiGroup test
+   */
+  'post /test/getui' => function($req, $res) {
+    require_once(EXTEND_PATH . 'GETUI/IGt.Push.php');
+    require_once(EXTEND_PATH . 'GETUI/igetui/IGt.AppMessage.php');
+    require_once(EXTEND_PATH . 'GETUI/igetui/IGt.APNPayload.php');
+    require_once(EXTEND_PATH . 'GETUI/igetui/template/IGt.BaseTemplate.php');
+    require_once(EXTEND_PATH . 'GETUI/IGt.Batch.php');
+    require_once(EXTEND_PATH . 'GETUI/igetui/utils/AppConditions.php');
+
+    // $HOST = 'http://118.24.248.160:9001';
+    $HOST = 'http://sdk.open.api.igexin.com/apiex.htm';
+    $APPID = '97El3h2UeD8BODhvS1OH7A';
+    $APPKEY = 'OCfGyJKYxX54QCIUr5WX94';
+    $MASTERSECRET = 'RwRIayE4C58LeyrIG2JN9A';
+
+    $igt = new IGeTui($HOST,$APPKEY,$MASTERSECRET);
+    //定义透传模板，设置透传内容，和收到消息是否立即启动启用
+    $template = IGtNotificationTemplateDemo();
+    //$template = IGtLinkTemplateDemo();
+    // 定义"AppMessage"类型消息对象，设置消息内容模板、发送的目标App列表、是否支持离线发送、以及离线消息有效期(单位毫秒)
+    $message = new IGtAppMessage();
+    $message->set_isOffline(true);
+    $message->set_offlineExpireTime(10 * 60 * 1000);//离线时间单位为毫秒，例，两个小时离线为3600*1000*2
+    $message->set_data($template);
+
+    $appIdList=array($APPID);
+    $phoneTypeList=array('ANDROID');
+    $provinceList=array('浙江');
+    $tagList=array('haha');
+    //用户属性
+    //$age = array("0000", "0010");
+
+
+    //$cdt = new AppConditions();
+   // $cdt->addCondition(AppConditions::PHONE_TYPE, $phoneTypeList);
+   // $cdt->addCondition(AppConditions::REGION, $provinceList);
+    //$cdt->addCondition(AppConditions::TAG, $tagList);
+    //$cdt->addCondition("age", $age);
+
+    $message->set_appIdList($appIdList);
+    //$message->set_conditions($cdt->getCondition());
+
+    $rep = $igt->pushMessageToApp($message,"任务组名");
+
+    return $rep;
   }
 ]
 ?>
