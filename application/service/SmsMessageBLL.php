fdbab7f5f42d99b032281108054941f44feb1c94
ruanjiayou <1439120442@qq.com>
2018-09-18 17:43:05 +0800

    修改

--- application/service/SmsMessageBLL.php
+++ application/service/SmsMessageBLL.php
@@ -31,7 +31,8 @@ class SmsMessageBLL extends BLL {
       'type' => 'required|string',
       'params' => 'required|array|default:array',
       'title' => 'empty|string|default:""',
-      'content' => 'empty|string|default:""'
+      'content' => 'empty|string|default:""',
+      'cid' => 'string|default:""'
     ]);
     $data = $validation->validate($input);
     $one = model('user')->where(['phone'=>$data['phone']])->count();
@@ -54,6 +55,28 @@ class SmsMessageBLL extends BLL {
       $data['title'] = $place['sign'];
       $data['content'] = $content;
       $result = $this->create($data);
+      $kv = [
+        'refused' => '邀请进度:邀请被拒绝',
+        'accepted2A' => '邀请进度:邀请被接受',
+        'accepted2C' => '邀请进度:邀请被接受',
+        'canceled2A' => '邀请进度:邀请被取消',
+        'canceled2C' => '邀请进度:邀请被取消',
+        'canceling2A' => '邀请进度:邀请被取消',
+        'canceling2C' => '邀请进度:邀请被取消',
+        'complaintA2AB' => '邀请进度:订单被投诉',
+        'complaintA2CB' => '邀请进度:订单被投诉',
+        'complaintC2AB' => '邀请进度:订单被投诉',
+        'complaintC2CB' => '邀请进度:订单被投诉',
+        'withdraw' => '提现通知:您发起了提现请求',
+        'confirmed' => '邀请进度:邀请被确认'
+      ];
+      if($data['cid']!="" && isset($kv[$place['place']])) {
+        return (new GeTui())->sendOne(
+          $data['phone'],
+          $data['cid'],
+          ['title'=>'[商务之星]', 'content'=> $content, 'payload'=> $content]
+        );
+      }
       return $result;
     } else if($place['signId'] == 0 || $place['tplId'] == 0) {
       thrower('sms', 'placeNotFound');
