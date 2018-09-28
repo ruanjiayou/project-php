50921dee27147d7661c824a2d7cc57703498381f
ruanjiayou <1439120442@qq.com>
2018-09-28 10:49:51 +0800

    修改task

--- application/task/default.php
+++ application/task/default.php
@@ -1,17 +1,42 @@
 <?php
 require __DIR__ . '/../../public/index.php';
 use think\Log;
-use app\model;
 // Log::write('test\\n');
-
-$invitations = (new TaskBLL())->getList(['limit' => 0, 'where' => ['type' => 'invitation']]);
-$ids = [];
-for($i=0;$i<count($invitations);$i++) {
-  Log::record($invitations[$i]);
-  array_push($ids, $invitations[$i]['id']);
-}
-Log::write($ids);
-
 // $fh = fopen('/home/d.txt', 'a+');
 // fwrite($fh,'123\n');
 // fclose($fh);
+
+/**
+ * 自动返现
+ * @param $id 邀请订单id
+ */
+function rebate($id) {
+  $invitation = db('invitation')->where(['id'=>$id])->find();
+  if(!empty($invitation)) {
+    Log::record($invitation);
+  } else {
+    Log::record('empty');
+  }
+}
+
+$tasks = db('task')->where(['type' => 'invitation'])->limit(0)->select();
+$ids = [];
+// success
+$invitationBLL = new \InvitationBLL();
+Log::write($invitationBLL);
+// fail getInfo()不存在?
+// $invitation = $invitationBLL->getInfo(33);
+// Log::write($invitation);
+// fail
+// $invitation = new \Invitation();
+// Log::write($invitation);
+for($i=0;$i<count($tasks);$i++) {
+  $task = $tasks[$i];
+  array_push($ids, $tasks[$i]['id']);
+  if($task['type'] === 'invitation') {
+    // rebate($task['taskId']);
+  }
+}
+if(!empty($ids)) {
+  db('task')->where(['id'=>['in', $ids]])->delete();
+}
