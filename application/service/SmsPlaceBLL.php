5814f9aa00cf9b804c1df0bf7ec592668cd17ada
ruanjiayou <1439120442@qq.com>
2018-10-08 18:23:48 +0800

    每个模板只能用一个地方

--- application/service/SmsPlaceBLL.php
+++ application/service/SmsPlaceBLL.php
@@ -23,10 +23,11 @@ class SmsPlaceBLL extends BLL {
     $this->update(['signId'=>$sign['logicId'],'sign'=>$sign['text']], ['place'=>$place['place']]);
     // 4.如果旧签名没有被引用,旧改为success,可以被删除
     if($oldId !==0 && $oldId !== $signId) {
-      $one = $this->getInfo(['signId'=>$oldId]);
-      if($one === null) {
-        $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
-      }
+      // $one = $this->getInfo(['signId'=>$oldId]);
+      // if($one === null) {
+      //   $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
+      // }
+      $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
     }
     return true;
   }
@@ -48,10 +49,11 @@ class SmsPlaceBLL extends BLL {
     $this->update(['tplId'=>$tpl['logicId'],'tpl'=>$tpl['text'],'isSms'=>1], ['place'=>$place['place']]);
     // 4.如果旧模板没有被引用,旧改为success,可以被删除
     if($oldId !== 0 && $oldId !== $tplId) {
-      $one = $this->getInfo(['tplId'=>$oldId]);
-      if($one === null) {
-        $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
-      }
+      // $one = $this->getInfo(['tplId'=>$oldId]);
+      // if($one === null) {
+      //   $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
+      // }
+      $smsBLL->edit(['status'=>'success'], ['logicId'=>$oldId]);
     }
     return true;
   }
